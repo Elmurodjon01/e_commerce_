8088765739d43212353fbb7531b00691da159ab5
Elmurodjon01 <abdulajonov01@gmail.com>
2023-04-18 17:39:02 +0900

    all

--- lib/src/features/authentication/presentation/account/account_screen.dart
+++ lib/src/features/authentication/presentation/account/account_screen.dart
@@ -1,37 +1,55 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Simple account screen showing some user info and a logout button.
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends ConsumerWidget {
   const AccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<void>>(
+      accountScreenControllorProvider,
+      (_, next) => next.showAlertDialogOnError(context),
+    );
+    final state = ref.watch(accountScreenControllorProvider);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account'.hardcoded),
+        title: state.isLoading
+            ? const CircularProgressIndicator()
+            : Text('Account'.hardcoded),
         actions: [
           ActionTextButton(
             text: 'Logout'.hardcoded,
-            onPressed: () async {
-              // get the navigator before the async gap
-              final navigator = Navigator.of(context);
-              final logout = await showAlertDialog(
-                context: context,
-                title: 'Are you sure?'.hardcoded,
-                cancelActionText: 'Cancel'.hardcoded,
-                defaultActionText: 'Logout'.hardcoded,
-              );
-              if (logout == true) {
-                // TODO: Sign out the user.
-                navigator.pop();
-              }
-            },
+            onPressed: state.isLoading
+                ? null
+                : () async {
+                    // get the navigator before the async gap
+                    final navigator = Navigator.of(context);
+                    final logout = await showAlertDialog(
+                      context: context,
+                      title: 'Are you sure?'.hardcoded,
+                      cancelActionText: 'Cancel'.hardcoded,
+                      defaultActionText: 'Logout'.hardcoded,
+                    );
+                    if (logout == true) {
+                      final success = await ref
+                          .read(accountScreenControllorProvider.notifier)
+                          .signOut();
+                      if (success) {
+                        // ignore: use_build_context_synchronously
+                        Navigator.of(context).pop();
+                      }
+                    }
+                  },
           ),
         ],
       ),
@@ -49,7 +67,7 @@ class UserDataTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final style = Theme.of(context).textTheme.subtitle2!;
+    final style = Theme.of(context).textTheme.titleSmall!;
     // TODO: get user from auth repository
     const user = AppUser(uid: '123', email: 'test@test.com');
     return DataTable(
